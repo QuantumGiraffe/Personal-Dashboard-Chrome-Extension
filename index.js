48b29759b09255eebe46dee866c3c5c3a39c4366
CosmicGiraffe <99769547+CosmicGiraffe@users.noreply.github.com>
2022-05-13 14:18:32 -0400

    Added Quote of the day

--- index.js
+++ index.js
@@ -4,25 +4,28 @@
 // BACKGROUND IMAGE-----------------------------------using Unsplash API 
 
 
-fetch("https://api.unsplash.com/photos/random?orientation=landscape&query=rocks&client_id=dw9oA4JSWT9Q3VVRz6QLu8rCfO_kcoFhUSurmw_WZoI")
+fetch("https://api.unsplash.com/photos/random?orientation=landscape&query=geology&client_id=dw9oA4JSWT9Q3VVRz6QLu8rCfO_kcoFhUSurmw_WZoI")
 
+    
     .then(res => res.json())
     .then(data => {
+        
         document.body.style.backgroundImage = `url(${data.urls.regular})`
-		document.getElementById("author").textContent = `By: ${data.user.name}`
+		document.getElementById("attribution").textContent = `Photo by ${data.user.name} on Unsplash`
     })
 
     // If fetch fails for some reason, use this predetermined image as a background:
     .catch(err => {
         document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
 )`
-		document.getElementById("author").textContent = `By: Dodi Achmad`
+		document.getElementById("attribution").textContent = `Photo by Dodi Achmad`
     })
 
 
 
 
 // CRYPTO COINS--------------------------------------using Coingecko API
+
 
 fetch("https://api.coingecko.com/api/v3/coins/ethereum")
     .then(res => {
@@ -32,11 +35,34 @@ fetch("https://api.coingecko.com/api/v3/coins/ethereum")
         return res.json()
     })
     .then(data => {
-        document.getElementById("crypto-top").innerHTML = `
+        document.getElementById("crypto-top-one").innerHTML = `
             <img src=${data.image.small} />
             <span>${data.name}</span>
         `
-        document.getElementById("crypto").innerHTML += `
+        document.getElementById("crypto-one").innerHTML += `
+            <p>🎯: $${data.market_data.current_price.usd}</p>
+            <p>👆: $${data.market_data.high_24h.usd}</p>
+            <p>👇: $${data.market_data.low_24h.usd}</p>
+        `
+    })
+    .catch(err => console.error(err))
+
+
+    // =======================================================
+
+    fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+    .then(res => {
+        if (!res.ok) {
+            throw Error("Something went wrong")
+        }
+        return res.json()
+    })
+    .then(data => {
+        document.getElementById("crypto-top-two").innerHTML = `
+            <img src=${data.image.small} />
+            <span>${data.name}</span>
+        `
+        document.getElementById("crypto-two").innerHTML += `
             <p>🎯: $${data.market_data.current_price.usd}</p>
             <p>👆: $${data.market_data.high_24h.usd}</p>
             <p>👇: $${data.market_data.low_24h.usd}</p>
